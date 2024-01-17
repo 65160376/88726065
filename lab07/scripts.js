2092cf3b75f716a2f57b9f6d025a013d38d5199f
65160376 <65160376@go.buu.ac.th>
2024-01-17 19:23:33 +0700

    ลบรายการ Todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -18,7 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
     todoInput.value = "";
     }
 }
-
+function deleteTodo(index) {
+    todos.splice(index, 1);
+    renderTodoList();
+    }
 
 function renderTodoList() {
     console.log(todos);
